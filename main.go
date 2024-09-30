a41939b03e093ccf0c4a3ba50bead1e9a2b2e31f
naufal.abiyyu <naufalabiyyu28@gmail.com>
2024-10-01 00:06:55 +0700

    feat: add function conversion from kelvin

--- main.go
+++ main.go
@@ -23,3 +23,13 @@ func FahrenheitToKelvin(suhuFahrenheit float64) float64 {
 	suhuKelvin := (suhuFahrenheit + 459.67) * (5 / 9)
 	return suhuKelvin
 }
+
+func KelvinToCelcius(suhuKelvin float64) float64 {
+	suhuCelcius := suhuKelvin - 273.15
+	return suhuCelcius
+}
+
+func KelvinToFahrenheit(suhuKelvin float64) float64 {
+	suhuFahrenheit := (suhuKelvin * (9 / 5)) - 459.67
+	return suhuFahrenheit
+}
