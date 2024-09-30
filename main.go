e1b65a1add316f89043d52714287dc0198234b9a
naufal.abiyyu <naufalabiyyu28@gmail.com>
2024-09-30 23:58:20 +0700

    feat: add function conversion from celcius

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
-	var suhu float64
-	fmt.Print("Masukan suhu: ")
-	fmt.Scanf("%f", &suhu)
-	fmt.Printf("Suhu yg di input adalah, %f\n", suhu)
 
-	var money uint64
-	fmt.Print("Masukan uang: ")
-	fmt.Scanf("%d", &money)
-	fmt.Printf("Uang yg di input adalah, %d\n", money)
+}
 
-	var name string
-	fmt.Print("Masukan nama: ")
-	fmt.Scanf("%s", &name)
-	fmt.Printf("Nama yg di input adalah, %s\n", name)
+func CelciusToFahrenheit(suhuCelcius float64) float64 {
+	suhuFahrenheit := (9 / 5 * suhuCelcius) + 32
+	return suhuFahrenheit
+}
+
+func CelciusToKelvin(suhuCelcius float64) float64 {
+	suhuKelvin := suhuCelcius + 273.15
+	return suhuKelvin
 }
